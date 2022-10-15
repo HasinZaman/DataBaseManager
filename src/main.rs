c0bf2529fbff1d915ab3d9d9f489121296de1c05
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-10-15 08:22:36 -0400

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::CrosstermBackend,
+    Terminal, widgets::{Block, Borders}
+};
+
+use crossterm::{
+    execute, terminal::{EnterAlternateScreen, LeaveAlternateScreen}
+};
+
+fn main() {
+}
