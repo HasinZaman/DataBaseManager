7ea0427f7e68a083e368e58a9dadb5c807c7c8e4
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-11-06 12:33:00 -0500

    implemented display for Table & AttributeType

--- src/backend/relation/table.rs
+++ src/backend/relation/table.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Clone)]
 pub struct Table{
     pub name: String,
@@ -12,6 +14,11 @@ pub struct Attribute{
     pub constraint: Vec<Constraint>
 }
 
+impl fmt::Display for Attribute {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {}", self.name, self.data_type)
+    }
+}
 
 #[derive(Clone)]
 pub enum Constraint{
@@ -60,4 +67,44 @@ pub enum AttributeType{
     TimeStamp,
     Time,
     Year
+}
+impl fmt::Display for AttributeType{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            //string data types
+            AttributeType::Char(val) => write!(f, "char({})", val),
+            AttributeType::VarChar(val) => write!(f, "varchar({})", val),
+            AttributeType::Binary(val) => write!(f, "binary({})", val),
+            AttributeType::VarBinary(val) => write!(f, "varbinary({})", val),
+            AttributeType::TinyBlob => write!(f, "tinyblob"),
+            AttributeType::TinyText => write!(f, "tinytext"),
+            AttributeType::Text(val) => write!(f, "text({})", val),
+            AttributeType::Blob(val) => write!(f, "blob({})", val),
+            AttributeType::MediumText => write!(f, "mediumtext"),
+            AttributeType::MediumBlob => write!(f, "mediumblob"),
+            AttributeType::LongText => write!(f, "longtext"),
+            AttributeType::LongBlob => write!(f, "longblob"),
+            AttributeType::Enum{val} => panic!(),
+            AttributeType::Set{val} => todo!(),
+
+            //numeric data types
+            AttributeType::Bit(val) => write!(f, "bit({})", val),
+            AttributeType::TinyInt(val) => write!(f, "tinyint({})", val),
+            AttributeType::Bool => write!(f, "bool"),
+            AttributeType::Boolean => write!(f, "boolean"),
+            AttributeType::SmallInt(val) => write!(f, "smallint({})", val),
+            AttributeType::MediumInt(val) => write!(f, "mediumint({})", val),
+            AttributeType::Int(val) => write!(f, "int({})", val),
+            AttributeType::BigInt(val) => write!(f, "bigint({})", val),
+            AttributeType::Float(val) => write!(f, "float({})", val),
+            AttributeType::Decimal(val_1 , val_2) => write!(f, "decmimal({},{})", val_1, val_2),
+
+            //Date time
+            AttributeType::Date => write!(f, "date"),
+            AttributeType::DateTime => write!(f, "DateTime"),
+            AttributeType::TimeStamp => write!(f, "TimeStamp"),
+            AttributeType::Time => write!(f, "Time"),
+            AttributeType::Year => write!(f, "Year"),
+        }
+    }
 }
