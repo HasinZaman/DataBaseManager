b2ead47a15a4ac008e9b65bbed3a2f43d486523c
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-11-07 17:03:00 -0500

    Update relation_page.rs
    - Added rustdoc
    - Impl Renderable for RelationPage

--- src/ui/pages/relations/relation_page.rs
+++ src/ui/pages/relations/relation_page.rs
@@ -1,19 +1,81 @@
-use tui::{layout::Rect, Frame, backend::CrosstermBackend};
+use tui::{layout::Rect, Frame, backend::CrosstermBackend, text::{Spans, Span}, widgets::{Paragraph, Block, Borders}};
 
-use crate::{backend::relation::Relation, ui::renderable::Renderable};
+use crate::{backend::relation::{Relation}, ui::renderable::Renderable};
 
+/// RelationPage struct handles the state required in order render a single relation
 pub struct RelationPage{
     pub relation: Relation
 }
 
 impl RelationPage {
-    fn new(relation: Relation) -> RelationPage {
+    /// new associative function initializes RelationPage
+    pub fn new(relation: Relation) -> RelationPage {
         RelationPage { relation: relation }
     }
 }
 
 impl Renderable for RelationPage{
     fn render<T: std::io::Write>(&self, display_area: Rect, frame: &mut Frame<CrosstermBackend<T>>) {
-        todo!()
+        let lines : Vec<Spans> = {
+            vec![
+                //define schema name line
+                vec![Spans::from(self.relation.name())],
+
+                //define relation definition lines
+                match &self.relation {
+                    Relation::Table(table) => {
+                        table.attributes
+                            .iter()
+                            .enumerate()
+                            .map(|(index, attr)| {
+                                Spans::from(vec![ 
+                                    {
+                                        if table.attributes.len() - 1 == index {//last attribute
+                                            Span::from("└")
+                                        }
+                                        else{
+                                            Span::from("├")
+                                        }
+                                    },
+                                    Span::from(" "),
+
+                                    Span::from(attr.name.clone()),
+                                    Span::from(" "),
+
+                                    Span::from(attr.data_type.to_string()),
+                                    Span::from(" "),
+
+                                    {
+                                        if Some(index) == table.primary_key {
+                                            Span::from("Primary Key ")
+                                        }
+                                        else{
+                                            Span::from("")
+                                        }
+                                    },
+                                    Span::from({
+                                        let tmp: Vec<String> = attr.constraint
+                                            .iter()
+                                            .map(|c| format!("{}", c.to_string()))
+                                            .collect();
+
+                                        tmp.join(", ")
+                                    })
+
+                                ])
+                            })
+                            .collect()
+                    },
+                    Relation::View(_veiw) => todo!()
+                }
+            ].concat()
+        };
+
+        frame.render_widget(
+            Paragraph::new(lines)
+                .block(
+                    Block::default().title("Relation Schema").borders(Borders::ALL)),
+            display_area
+        );
     }
 }
