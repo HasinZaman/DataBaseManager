619c76ccacf693d9a8737c3cbe2ef44b239b584d
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-11-07 11:32:22 -0500

    Update renderable.rs
    - Removed Unused use
    - Added rust doc to Renderable trait

--- src/ui/renderable.rs
+++ src/ui/renderable.rs
@@ -4,9 +4,7 @@ use tui::{
     Frame
 };
 
-// use crossterm::{
-//     execute, terminal::{EnterAlternateScreen, LeaveAlternateScreen}
-// };
+/// Renderable trait defines methods required to render a widget in a display area
 pub trait Renderable {
     fn render<T: std::io::Write>(&self, display_area: Rect, frame: &mut Frame<CrosstermBackend<T>>);
 }
