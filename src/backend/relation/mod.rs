8b79a67daccb75507d9912a37302eb827dc5fa30
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-11-07 17:02:11 -0500

    Update mod.rs
    - impl Relation
      -  name method allows for easy access to name variable of relation from table or query

--- src/backend/relation/mod.rs
+++ src/backend/relation/mod.rs
@@ -10,3 +10,13 @@ pub enum Relation{
     Table(Table),
     View(Query),
 }
+
+impl Relation {
+    /// name method allows for easy access to name of relation from table or query
+    pub fn name(&self) -> String {
+        match self {
+            Relation::Table(table) => table.name.clone(),
+            Relation::View(_view) => todo!()
+        }
+    }
+}
