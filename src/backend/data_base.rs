450f39d04b4bea51ba97becbdca6ea6b5f95136f
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-11-12 15:07:24 -0500

    Update data_base.rs

--- src/backend/data_base.rs
+++ src/backend/data_base.rs
@@ -1,6 +1,17 @@
-use std::{fmt, env};
+use std::{fmt, env::{self, VarError}};
 
 use mysql::{prelude::*, Opts, Conn, Row, Error};
+
+macro_rules! load_env_var {
+    ($key : literal) => {
+        match env::var($key) {
+            Err(err) => {
+                return Err(err);
+            }
+            Ok(ok) => ok,
+        }
+    };
+}
 
 #[derive(Debug)]
 pub struct DataBase {
@@ -11,51 +22,32 @@ pub struct DataBase {
     password: String,
 }
 
-macro_rules! env_var_to_variable {
-    ($key : literal, $var : ident) => {
-        match env::var($key) {
-            Err(_err) => {
-                println!("{} not assigned", $key);
-                return Option::None;
-            }
-            Ok(ok) => $var = ok,
-        }
-    };
-}
-
 impl DataBase {
     pub fn new(host: String, port: String, name: String, username: String, password: String) -> DataBase {
         DataBase { host: host, port: port, name: name, username: username, password: password }
     }
-    pub fn from_env() -> Option<DataBase> {
-        let db_host: String;
-        let db_port: String;
-        let db_name: String;
-        let db_username: String;
-        let db_password: String;
 
-        env_var_to_variable!("DB_host", db_host);
-        env_var_to_variable!("DB_port", db_port);
-        env_var_to_variable!("DB_name", db_name);
-        env_var_to_variable!("DB_username", db_username);
-        env_var_to_variable!("DB_password", db_password);
-
-        Option::Some(
-            DataBase::new(db_host, db_port, db_name, db_username, db_password)
-        )
+    pub fn from_env() -> Result<DataBase, VarError> {
+        Ok(DataBase {
+            host: load_env_var!("DB_host"),
+            port: load_env_var!("DB_port"),
+            name: load_env_var!("DB_name"),
+            username: load_env_var!("DB_username"),
+            password: load_env_var!("DB_password"),
+        })
     }
+
     fn get_conn(&self) -> mysql::Conn {
         let url = format!(
             "mysql://{}:{}@{}:{}/{}",
             self.username, self.password, self.host, self.port, self.name
         );
 
-        println!("{}", url);
-
         let url: Opts = Opts::from_url(&url).unwrap();
 
         Conn::new(url).unwrap()
     }
+    
     pub fn execute<E>(&self, command: &str, row_map: fn(row: Result<Row, Error>) -> E ) -> Result<Vec<E>, Error> {
         let mut conn = self.get_conn();
 
@@ -74,6 +66,7 @@ impl DataBase {
         Ok(rows)
     }
 }
+
 impl fmt::Display for DataBase {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
