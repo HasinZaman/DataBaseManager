548d49b5e8cf866ba2574c4db2ad241188b7cc9d
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-11-17 22:11:12 -0500

    Implemented Renderable for QueryPage

--- src/ui/pages/schema.rs
+++ src/ui/pages/schema.rs
@@ -1,21 +1,232 @@
-use tui::{Frame,backend::CrosstermBackend, layout::Rect};
+use std::cmp::{max, min};
 
-use crate::ui::renderable::Renderable;
+use regex::{Regex, Captures};
+use tui::{Frame,backend::CrosstermBackend, layout::{Rect, Constraint}, widgets::{Table, Row, Cell, Block, Borders},};
 
-pub struct SchemaListPage{
+use crate::{ui::renderable::Renderable, backend::{query::{Query, QueryError}, data_base::DataBase}};
+
+struct QueryCache{
+    pub start_col: usize,
+    pub columns: Vec<(String, usize)>,
+    pub rows: Vec<Vec<String>>
 }
 
-impl Renderable for SchemaListPage{
-    fn render<T: std::io::Write>(&self, display_area: Rect, frame: &mut Frame<CrosstermBackend<T>>) {
-        todo!()
+pub struct QueryPage<'a>{
+    query: &'a Query,
+    query_offset: usize,
+    query_cache: QueryCache
+}
+
+impl<'a> QueryPage<'a> {
+    pub fn new(query: &'a Query) -> Result<QueryPage<'a>, QueryError> {
+        if let Query::Select(_select_query) = query {
+            return Ok(
+                QueryPage {
+                    query: query,
+                    query_offset: 0,
+                    query_cache: QueryCache {
+                        start_col: 0,
+                        columns: Vec::new(),
+                        rows: Vec::new()
+                    }
+                }
+            )
+        }
+
+        Err(
+            QueryError::InvalidQuery{
+                expected_variant : Query::Select(
+                    String::from("")
+                )
+            }
+        )
+    }
+
+    pub fn next_row(&mut self, skip: usize) -> &mut Self{
+        self.query_offset += skip;
+
+        self
+    }
+    
+    pub fn prev_row(&mut self, skip: usize) -> &mut Self{
+        match self.query_offset.checked_sub(skip) {
+            Some(new_offset) => self.query_offset = new_offset,
+            None => self.query_offset = 0usize,
+        }
+
+        self
+    }
+
+    pub fn start_col(&mut self, start_col: usize) -> &mut Self{
+        self.query_cache.start_col = start_col;
+        self
+    }
+
+    pub fn update(&mut self, row_count: usize) -> &mut Self{
+        match DataBase::from_env() {
+            Ok(db) => {
+
+                let mut columns: Vec<(String, usize)> = Vec::new();
+                let mut rows: Vec<Vec<String>> = Vec::new();
+
+                let _result: Result<Vec<Option<bool>>, _> = db.execute(
+                    &self.get_query(row_count).to_string(),
+                    |row| {
+                        let row = row.unwrap();
+
+                        let mut row_tmp: Vec<String> = Vec::new();
+
+                        row.clone()
+                            .unwrap()
+                            .iter()
+                            .zip(
+                                row.columns()
+                                    .iter()
+                            )
+                            .enumerate()
+                            .map(|(i1, (val, col))| (i1, val, col))
+                            .for_each(|(i1, val, col)| {
+                                let val = format!("{:?}", val);
+
+                                match columns.get(i1) {
+                                    Some((_name, max_size)) => {
+                                        columns[i1].1 = max(*max_size, val.len());
+                                    },
+                                    None => {
+                                        columns.push({
+                                            let col_name = col.name_str();
+                                            (
+                                                col_name.to_string(),
+                                                max(
+                                                    col_name.len(),
+                                                    val.len()
+                                                )
+                                                
+                                            )
+                                        })
+                                    }
+                                }
+                                
+                                row_tmp.push(val);
+                            });
+                        
+                        rows.push(row_tmp);
+
+                        None
+                    }
+                );
+
+                self.query_cache = QueryCache {
+                    start_col: min(self.query_cache.start_col, columns.len()-1),
+                    columns: columns,
+                    rows: rows
+                }
+            },
+            Err(_err) => {
+                todo!()
+            }
+        }
+
+        self
+    }
+
+    fn get_query(&self, row_count: usize) -> Query {
+        let exist_opp = Regex::new("^[Ss][Ee][Ll][Ee][Cc][Tt] .+ [Ll][Ii][Mm][Ii][Tt] (\\d+),?([\\d]+)?$").unwrap();
+
+        let tmp_query = self.query.to_string();
+
+        Query::Select(
+            match exist_opp.is_match(&tmp_query) {
+                true => {
+                    let captures = exist_opp.captures(&tmp_query).unwrap();
+                
+                    exist_opp.replace(
+                        &tmp_query,
+                        |_cap: &Captures| {
+                            format!(
+                                "{}{},{}",
+                                &tmp_query[
+                                    0
+                                    ..
+                                    {
+                                        let tmp = captures.get(1).unwrap();
+
+                                        tmp.start()
+                                    }
+                                ],
+                                self.query_offset,
+                                row_count
+                            )
+                        }
+                    ).to_string()
+                }
+                false => {
+                    format!("{} LIMIT {},{}", tmp_query, self.query_offset, row_count)
+                }
+            }
+        )
     }
 }
 
-pub struct QueryPage{
-}
-
-impl Renderable for QueryPage{
+impl<'a> Renderable for QueryPage<'a>{
     fn render<T: std::io::Write>(&self, display_area: Rect, frame: &mut Frame<CrosstermBackend<T>>) {
-        todo!()
+        let height: usize = (display_area.height - 2 - 1) as usize; 
+
+        let constraints : Vec<Constraint>;
+
+        let table: Table = Table::new(
+            self.query_cache
+                .rows
+                .iter()
+                .map(
+                    |columns| {
+                        Row::new({
+                                let cells: Vec<Cell> = columns.iter()
+                                    .skip(self.query_cache.start_col)
+                                    .map(
+                                        |col| {
+                                            Cell::from(col.to_string())
+                                        }
+                                    ).collect();
+
+                                cells
+                            }
+                        )
+                    }
+                )
+        ).header(
+            Row::new({
+                let tmp: Vec<Cell> = self.query_cache
+                    .columns
+                    .iter()
+                    .skip(self.query_cache.start_col)
+                    .map(|name| Cell::from(name.0.to_string()))
+                    .collect();
+
+                    tmp
+                }
+            )
+        ).widths({
+                constraints = self.query_cache
+                    .columns
+                    .iter()
+                    .skip(self.query_cache.start_col)
+                    .map(|col| Constraint::Length(col.1 as u16))
+                    .collect();
+
+                &constraints
+            }
+        ).column_spacing(3)
+        .block(
+            Block::default()
+            .title(format!(
+                "Query: {}",
+                self.get_query(height).to_string()
+            ))
+            .borders(Borders::ALL)
+        );
+
+
+        frame.render_widget(table, display_area);
     }
 }
