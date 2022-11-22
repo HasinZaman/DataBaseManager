8879c03e0f865eed0003fbed28139d1586c8ee2e
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-11-22 12:34:10 -0500

    Update snapshot.rs
    - Created Error struct for loading and saving SnapShots
    - Created SnapShot Struct
    - Created SanpShotsFile Struct
      -> Created open associative function
      -> Created save method

--- src/ui/pages/snapshot.rs
+++ src/ui/pages/snapshot.rs
@@ -1,7 +1,86 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+use ron::{error::SpannedError};
+use serde::{Deserialize, Serialize};
+use time::{Time, Date};
 use tui::{Frame,backend::CrosstermBackend, layout::Rect};
+
 use crate::ui::renderable::Renderable;
 
-struct SnapShotPage{
+#[derive(Debug)]
+enum Error {
+    FileOpenErr(std::io::Error),
+    FileReadErr(std::io::Error),
+    FileWriteErr(std::io::Error),
+    DeSerializationErr(SpannedError),
+    Err(String)
+}
+
+#[derive(Deserialize, Serialize)]
+struct SnapShot{
+    date: (Date, Time),
+    path: String,
+}
+
+#[derive(Deserialize, Serialize)]
+struct SnapShotsFile{
+    name: String,
+    snap_shots: Vec<SnapShot>,
+}
+
+impl SnapShotsFile{
+    const SNAP_SHOT_NAME : &str = "snap_shots.ron";
+
+    pub fn open(file_name: &str) -> Result<SnapShotsFile, Error> {
+        let mut file: File = match File::open(file_name) {
+            Ok(file) => file,
+            Err(err) => return Err(Error::FileOpenErr(err))
+        };
+
+        let contents: String = {
+            let mut contents: String = String::new();
+
+            if let Err(err) = file.read_to_string(&mut contents) {
+                return Err(Error::FileReadErr(err))
+            };
+
+            contents
+        };
+
+        match ron::from_str(&contents) {
+            Ok(val) => Ok(val),
+            Err(err) => Err(Error::DeSerializationErr(err))
+        }
+    }
+
+    pub fn save(&self) -> Result<(), Error> {
+        let mut file : File = match File::create(&self.name) {
+            Ok(file) => file,
+            Err(err) => return Err(Error::FileOpenErr(err))
+        };
+
+        let buffer = ron::to_string(&self).unwrap();
+        let buffer = buffer.as_bytes();
+        
+        match file.write(buffer) {
+            Ok(written_bytes) => {
+                if written_bytes == buffer.len() {
+                    return Ok(())
+                }
+                todo!();
+                return Err(Error::Err(String::from("")))
+            },
+            Err(err) => return Err(Error::FileWriteErr(err)),
+        }
+
+    }
+
+}
+
+
+pub struct SnapShotPage{
+}
 }
 
 impl Renderable for SnapShotPage{
