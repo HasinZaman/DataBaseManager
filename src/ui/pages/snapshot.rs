f2d721c98288424f192bdf4db639cb250b2cca5a
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-11-25 13:25:53 -0500

    Update snapshot.rs
    - Updated imports
    - Added Error variant
    - Made SnapShot struct public
    - Impl SnapShot
      - new constructor
    - Impl Hash for SnapShot
    - Impl PartialEq for SnapShot
    - Added update method to SnapShotsFile
    - Impl Default for SnapShotsFile
    - Derive Default for SnapShotPage
    - Impl SnapShotPage
      - new constructor
      - update
       - Update the SnapShotPagestruct with the current values in file
      - add
       - Add snapshot from SnapShotsFile
      - del
       - Delete snapshot from SnapShotsFile
      - save
       - Utility method to save SnapShotFile
    - Impl Renderable for SnapShotPage

--- src/ui/pages/snapshot.rs
+++ src/ui/pages/snapshot.rs
@@ -1,15 +1,32 @@
-use std::fs::File;
-use std::io::prelude::*;
+use std::{
+    cmp::max,
+    collections::HashSet,
+    hash::{Hash, Hasher},
+    {fs::File},
+    io::prelude::*
+};
 
 use ron::{error::SpannedError};
-use serde::{Deserialize, Serialize};
-use time::{Time, Date};
-use tui::{Frame,backend::CrosstermBackend, layout::Rect};
+
+use serde::{
+    Deserialize,
+    Serialize
+};
+
+use time::{OffsetDateTime};
+
+use tui::{
+    layout::{Constraint, Rect},
+    text::Span,
+    widgets::{Table, Row, Cell, Block, Borders},
+    {Frame,backend::CrosstermBackend}
+};
 
 use crate::ui::renderable::Renderable;
 
 #[derive(Debug)]
-enum Error {
+pub enum Error {
+    PathDoesNotExist,
     FileOpenErr(std::io::Error),
     FileReadErr(std::io::Error),
     FileWriteErr(std::io::Error),
@@ -17,21 +34,49 @@ enum Error {
     Err(String)
 }
 
-#[derive(Deserialize, Serialize)]
-struct SnapShot{
-    date: (Date, Time),
+#[derive(Deserialize, Serialize, Eq, Clone)]
+pub struct SnapShot{
+    time_stamp: OffsetDateTime,
     path: String,
+}
+
+impl SnapShot {
+    pub fn new(time_stamp: OffsetDateTime, path: String) -> Result<SnapShot, Error> {
+        Ok(
+            SnapShot {
+                time_stamp: time_stamp,
+                path: {
+                    if !std::path::Path::new(&path).exists() {
+                        return Err(Error::PathDoesNotExist); 
+                    }
+
+                    path
+                }
+            }
+        )
+    }
+}
+
+impl Hash for SnapShot {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.path.hash(state);
+    }
+}
+
+impl PartialEq for SnapShot {
+    fn eq(&self, other: &Self) -> bool {
+        self.path == other.path
+    }
 }
 
 #[derive(Deserialize, Serialize)]
 struct SnapShotsFile{
     name: String,
-    snap_shots: Vec<SnapShot>,
+    snap_shots: HashSet<SnapShot>,
 }
 
-impl SnapShotsFile{
-    const SNAP_SHOT_NAME : &str = "snap_shots.ron";
 
+impl SnapShotsFile{
     pub fn open(file_name: &str) -> Result<SnapShotsFile, Error> {
         let mut file: File = match File::open(file_name) {
             Ok(file) => file,
@@ -52,6 +97,12 @@ impl SnapShotsFile{
             Ok(val) => Ok(val),
             Err(err) => Err(Error::DeSerializationErr(err))
         }
+    }
+
+    pub fn update(&mut self) {
+        let tmp = SnapShotsFile::open(&self.name).unwrap();
+
+        self.snap_shots = tmp.snap_shots;
     }
 
     pub fn save(&self) -> Result<(), Error> {
@@ -75,16 +126,171 @@ impl SnapShotsFile{
         }
 
     }
-
 }
 
+impl Default for SnapShotsFile{
+    fn default() -> Self {
+        let path = "snap_shots.ron";
+        let snap_shot_file = SnapShotsFile::open(path);
 
+        match snap_shot_file {
+            Ok(val) => val,
+            Err(err) => {
+                if let Error::FileOpenErr(_err) = err {
+                    let snap_shot_file = Self {
+                        name: String::from(path),
+                        snap_shots: HashSet::new(),
+                    };
+                    
+                    let _result = snap_shot_file.save();
+    
+                    snap_shot_file
+                }
+                else{
+                    todo!()
+                }
+            }
+        }
+    }
+}
+
+#[derive(Default)]
 pub struct SnapShotPage{
+    snap_shots_file: SnapShotsFile
 }
+
+impl SnapShotPage{
+    pub fn new(file: &str) -> Result<SnapShotPage, Error> {
+        let file = SnapShotsFile::open(file)?;
+        Ok(
+            SnapShotPage{
+                snap_shots_file: file
+            }
+        )
+    }
+
+    pub fn update(& mut self) {
+        self.snap_shots_file.update();
+    }
+
+    pub fn add(& mut self, snap_shot: SnapShot) -> & mut Self{
+        self.snap_shots_file
+            .snap_shots
+            .insert(snap_shot);
+        
+        self.save();
+        self
+    }
+
+    pub fn del(& mut self, path: &str) -> Result<& mut Self, Error>{
+
+        let del_snap_shot = {
+            let val = self.snap_shots_file
+            .snap_shots
+            .iter()
+            .filter(|s| s.path == path)
+            .next();
+
+            match val {
+                Some(val) => Some(val.clone()),
+                None => None
+            }
+        };
+
+        match del_snap_shot {
+            Some(val) => {
+                self.snap_shots_file
+                    .snap_shots
+                    .remove(&val);
+            
+                    self.save();
+                Ok(self)
+            },
+            None => Err(Error::PathDoesNotExist)
+        }
+    }
+
+    fn save(&self) {
+        if let Err(_err) = self.snap_shots_file.save() {
+            //error pop notification
+            todo!()
+        }
+    }
+
 }
 
 impl Renderable for SnapShotPage{
     fn render<T: std::io::Write>(&self, display_area: Rect, frame: &mut Frame<CrosstermBackend<T>>) {
-        todo!()
+        let mut widths: [Constraint;2] = [Constraint::Length(10), Constraint::Length(4)];
+
+        let table: Table = Table::new(
+                {
+                    let rows: Vec<Row> =self.snap_shots_file
+                        .snap_shots
+                        .iter()
+                        .map(|snap_shot| {
+                            Row::new(
+                                vec![
+                                    Cell::from(
+                                        Span::from(
+                                            {
+                                                let tmp_str = snap_shot.time_stamp.to_string();
+
+                                                widths[0] = match widths[0] {
+                                                    Constraint::Length(l) => {
+                                                        Constraint::Length(max(l, tmp_str.len() as u16))
+                                                    }
+                                                    _ => {
+                                                        panic!()
+                                                    }
+                                                };
+
+                                                tmp_str
+                                            }
+                                            
+                                        )
+                                    ),
+                                    Cell::from(
+                                        Span::from(
+                                            {
+                                                let tmp_str = snap_shot.path.to_string();
+                                                widths[1] = match widths[1] {
+                                                    Constraint::Length(l) => {
+                                                        Constraint::Length(max(l, tmp_str.len() as u16))
+                                                    }
+                                                    _ => {
+                                                        panic!()
+                                                    }
+                                                };
+
+                                                tmp_str
+                                            }
+                                        )
+                                    ),
+                                ]
+                            )
+                        })
+                        .collect();
+
+                    rows
+                }
+            ).header(
+                Row::new(
+                    vec![
+                        Cell::from("Time Stamp"),
+                        Cell::from("Path")
+                    ]
+                )
+            ).widths(&widths)
+            .column_spacing(3)
+            .block(
+                Block::default()
+                    .title("Snap Shots")
+                    .borders(Borders::ALL)
+            );
+
+
+
+        frame.render_widget(table, display_area);
     }
 }
