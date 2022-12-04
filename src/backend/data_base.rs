56bcf8aaf4644efdfc3fd7f58271a09ac2669833
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-12-04 15:36:18 -0500

    Update data_base.rs
    Testing
    - Testing transaction functionality
    - Create DB testing environment
    - Test multiple sequential execution
    - Test single execution
    Database
    - Removed unnecessary unwraps

--- src/backend/data_base.rs
+++ src/backend/data_base.rs
@@ -60,9 +60,9 @@ impl DataBase {
     pub fn execute<E, F>(&self, command: &SQL, row_map: F ) -> Result<Vec<E>, Error> where F : FnMut(Result<Row, Error>) -> E{
         let mut conn = self.get_conn();
 
-        let mut tx = conn.start_transaction(TxOpts::default()).unwrap();
+        let mut tx = conn.start_transaction(TxOpts::default())?;
 
-        let statement = tx.prep(command.to_string()).unwrap();
+        let statement = tx.prep(command.to_string())?;
 
         let mut rows: Vec<E> = Vec::new();
 
@@ -91,10 +91,10 @@ impl DataBase {
     pub fn execute_multiple(&self, commands: &Vec<SQL>) -> Result<(), Error> {
         let mut conn = self.get_conn();
 
-        let mut tx = conn.start_transaction(TxOpts::default()).unwrap();
+        let mut tx = conn.start_transaction(TxOpts::default())?;
 
         for sql in commands{
-            let statement = tx.prep(sql.to_string()).unwrap();
+            let statement = tx.prep(sql.to_string())?;
 
             if let Err(err) = tx.exec_iter(&statement, ()) {
                 //let _result = &tx.rollback();
@@ -103,9 +103,8 @@ impl DataBase {
             
             let _result = tx.close(statement);
         }
+
         let _result = tx.commit();
-
-
         Ok(())
     }
 }
@@ -122,4 +121,279 @@ impl fmt::Display for DataBase {
             self.password
         )
     }
+}
+
+mod tests{
+    use std::{fs::{File, self}, io::Write};
+
+    use crate::backend::sql::SQL;
+
+    use super::DataBase;
+
+    struct FileEnv{
+        file_name: String
+    }
+    impl FileEnv{
+        pub fn new(file_name: &str, content: &str) -> FileEnv {
+
+            let mut file = File::create(&file_name).unwrap();
+            file.write(&content.as_bytes()).unwrap();
+
+            FileEnv{ file_name: file_name.to_string() }
+        }
+    }
+    impl Drop for FileEnv{
+        fn drop(&mut self) {
+            println!("Dropping {}", &self.file_name);
+            fs::remove_file(&self.file_name).unwrap();
+        }
+    }
+
+    struct DbEnv{
+        undo: Vec<SQL>
+    }
+    impl DbEnv{
+        pub fn new(set_up: Vec<SQL>, undo: Vec<SQL>) -> DbEnv {
+            let tmp = DbEnv { undo: undo };
+            
+            let db = DataBase::from_env().unwrap();
+
+            db.execute_multiple(&set_up).unwrap();
+
+            tmp
+        }
+    }
+    impl Drop for DbEnv{
+        fn drop(&mut self) {
+            let db = DataBase::from_env().unwrap();
+
+            db.execute_multiple(&self.undo).unwrap();
+        }
+    }
+
+    #[test]
+    fn execute_multiple_success() {
+        let _env = DbEnv::new(
+            vec![
+                SQL::from("CREATE TABLE execute_multiple_success (col1 INT)").unwrap(),
+                SQL::from("INSERT INTO execute_multiple_success (col1) VALUES (1)").unwrap(),
+            ],
+            vec![
+                SQL::from("DROP TABLE execute_multiple_success").unwrap(),
+            ]
+        );
+
+        let cmds = vec![
+            SQL::from("INSERT INTO execute_multiple_success (col1) VALUES (2)").unwrap(),
+            SQL::from("INSERT INTO execute_multiple_success (col1) VALUES (3)").unwrap(),
+            SQL::from("INSERT INTO execute_multiple_success (col1) VALUES (4)").unwrap(),
+        ];
+
+        let db = DataBase::from_env().unwrap();
+
+        let result = db.execute_multiple(&cmds);
+        assert!(result.is_ok());
+
+        let mut actual = db.execute(
+            &SQL::from("SELECT * FROM execute_multiple_success").unwrap(),
+            |row| {
+                let row = row.unwrap();
+
+                let val: i32 = row.get(0).unwrap();
+                
+                val
+            }
+        ).unwrap();
+
+        actual.sort();
+
+        assert_eq!(
+            actual,
+            vec![1,2,3,4]
+        );
+    }
+
+    #[test]
+    fn execute_one_success() {
+        let _env = DbEnv::new(
+            vec![
+                SQL::from("CREATE TABLE execute_one_success (col1 INT)").unwrap(),
+                SQL::from("INSERT INTO execute_one_success (col1) VALUES (1)").unwrap(),
+            ],
+            vec![
+                SQL::from("DROP TABLE execute_one_success").unwrap(),
+            ]
+        );
+
+        let cmd = SQL::from("INSERT INTO execute_one_success (col1) VALUES (2)").unwrap();
+
+        let db = DataBase::from_env().unwrap();
+
+        let result = db.execute(&cmd, |_| ());
+        assert!(result.is_ok());
+
+        let mut actual = db.execute(
+            &SQL::from("SELECT * FROM execute_one_success").unwrap(),
+            |row| {
+                let row = row.unwrap();
+
+                let val: i32 = row.get(0).unwrap();
+                
+                val
+            }
+        ).unwrap();
+
+        actual.sort();
+
+        assert_eq!(
+            actual,
+            vec![1,2]
+        );
+    }
+    
+    #[test]
+    fn execute_multiple_fail() {
+        let _env = DbEnv::new(
+            vec![
+                SQL::from("CREATE TABLE execute_multiple_fail (col1 INT, PRIMARY KEY(col1))").unwrap(),
+                SQL::from("INSERT INTO execute_multiple_fail (col1) VALUES (1)").unwrap(),
+            ],
+            vec![
+                SQL::from("DROP TABLE execute_multiple_fail").unwrap(),
+            ]
+        );
+
+        //syntax error
+        {
+            let cmds = vec![
+                SQL::from("INSERT INTO execute_multiple_fail (col1) VALUES (2)").unwrap(),
+                SQL::from("INSERT INTO execute_multiple_fail (col1) VALUES (3))").unwrap(),
+                SQL::from("INSERT INTO execute_multiple_fail (col1) VALUES (4)").unwrap(),
+            ];
+            let db = DataBase::from_env().unwrap();
+
+            let result = db.execute_multiple(&cmds);
+            assert!(result.is_err());
+
+            let mut actual = db.execute(
+                &SQL::from("SELECT * FROM execute_multiple_fail").unwrap(),
+                |row| {
+                    let row = row.unwrap();
+
+                    let val: i32 = row.get(0).unwrap();
+                    
+                    val
+                }
+            ).unwrap();
+
+            actual.sort();
+
+            assert_eq!(
+                actual,
+                vec![1]
+            );
+        }
+        //db constraint violation
+        {
+            let cmds = vec![
+                SQL::from("INSERT INTO execute_multiple_fail (col1) VALUES (2)").unwrap(),
+                SQL::from("INSERT INTO execute_multiple_fail (col1) VALUES (3)").unwrap(),
+                SQL::from("INSERT INTO execute_multiple_fail (col1) VALUES (4)").unwrap(),
+                SQL::from("INSERT INTO execute_multiple_fail (col1) VALUES (4)").unwrap(),
+            ];
+            let db = DataBase::from_env().unwrap();
+
+            let result = db.execute_multiple(&cmds);
+            assert!(result.is_err());
+
+            let mut actual = db.execute(
+                &SQL::from("SELECT * FROM execute_multiple_fail").unwrap(),
+                |row| {
+                    let row = row.unwrap();
+
+                    let val: i32 = row.get(0).unwrap();
+                    
+                    val
+                }
+            ).unwrap();
+
+            actual.sort();
+
+            assert_eq!(
+                actual,
+                vec![1]
+            );
+        }
+        
+
+        
+    }
+
+    #[test]
+    fn execute_one_fail() {
+        let _env = DbEnv::new(
+            vec![
+                SQL::from("CREATE TABLE execute_one_fail (col1 INT)").unwrap(),
+                SQL::from("INSERT INTO execute_one_fail (col1) VALUES (1)").unwrap(),
+            ],
+            vec![
+                SQL::from("DROP TABLE execute_one_fail").unwrap(),
+            ]
+        );
+        //syntax error
+        {
+            let cmd = SQL::from("INSERT INTO execute_one_fail (col1) VALUES (2))").unwrap();
+
+            let db = DataBase::from_env().unwrap();
+
+            let result = db.execute(&cmd, |_| ());
+            assert!(result.is_err());
+
+            let mut actual = db.execute(
+                &SQL::from("SELECT * FROM execute_one_fail").unwrap(),
+                |row| {
+                    let row = row.unwrap();
+
+                    let val: i32 = row.get(0).unwrap();
+                    
+                    val
+                }
+            ).unwrap();
+
+            actual.sort();
+
+            assert_eq!(
+                actual,
+                vec![1]
+            );
+        }
+        //constraint violation
+        {
+            let cmd = SQL::from("INSERT INTO execute_one_fail (col1) VALUES (1))").unwrap();
+
+            let db = DataBase::from_env().unwrap();
+
+            let result = db.execute(&cmd, |_| ());
+            assert!(result.is_err());
+
+            let mut actual = db.execute(
+                &SQL::from("SELECT * FROM execute_one_fail").unwrap(),
+                |row| {
+                    let row = row.unwrap();
+
+                    let val: i32 = row.get(0).unwrap();
+                    
+                    val
+                }
+            ).unwrap();
+
+            actual.sort();
+
+            assert_eq!(
+                actual,
+                vec![1]
+            );
+        }
+    }
+    
 }
