a4611a97f56b7a321e614d7278395f044429a478
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-12-05 13:49:43 -0500

    Created Pages enum

--- src/ui/pages/mod.rs
+++ src/ui/pages/mod.rs
@@ -1,3 +1,23 @@
+use super::renderable::Renderable;
+
 pub mod relations;
 pub mod schema;
 pub mod snapshot;
+
+pub enum Pages {
+    RelationList(relations::relation_list::RelationListPage),
+    Relation(relations::relation_page::RelationPage),
+    Query(schema::QueryPage),
+    SnapShot(snapshot::SnapShotPage)
+}
+
+impl Renderable for Pages{
+    fn render<T: std::io::Write>(&self, display_area: tui::layout::Rect, frame: &mut tui::Frame<tui::backend::CrosstermBackend<T>>) {
+        match self {
+            Pages::RelationList(val) => val.render(display_area, frame),
+            Pages::Relation(val) => val.render(display_area, frame),
+            Pages::Query(val) => val.render(display_area, frame),
+            Pages::SnapShot(val) => val.render(display_area, frame),
+        }
+    }
+}
