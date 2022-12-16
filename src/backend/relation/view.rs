44e4fa97bfcda11258464c771741bb380bbe7659
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-12-16 17:06:50 -0500

    Update view.rs
    - Added documentation to view.rs

--- src/backend/relation/view.rs
+++ src/backend/relation/view.rs
@@ -7,11 +7,14 @@ use super::RelationMethods;
 
 #[derive(Clone, Debug)]
 pub struct View{
+    /// The name of the view.
     pub name: String,
+    /// The query defining the view.
     pub query: QDL
 }
 
 impl View {
+    /// Returns a `View` created from a database with the given name.
     pub fn from_db(name: &str) -> Option<View> {
         let db = DataBase::from_env().unwrap();
 
@@ -47,6 +50,7 @@ impl View {
 
     }
 
+    /// Returns a new `View` with the given name and query.
     pub fn new(name: &str, query: QDL) -> View {
         View{
             name: name.to_string(),
@@ -56,13 +60,15 @@ impl View {
 }
 
 impl RelationMethods for View{
+    /// Returns a `QDL` representing a `SELECT` statement for the view.
     fn select(&self) -> QDL {
         QDL(format!("SELECT * FROM {}", self.name))
     }
+    /// Returns a `DDL` representing a `DROP` statement for the view.
     fn drop(&self) -> DDL{
         DDL(format!("DROP VIEW {}", self.name))
     }
-
+    /// Returns a `DDL` representing a `CREATE` statement for the view.
     fn create(&self) -> DDL {
         SQL::new(&format!("CREATE VIEW {} AS {}", self.name, *self.query)).unwrap().ddl().unwrap().clone()
     }
