4c08b720ff29a51b9398e106a890743141bacbd7
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-12-16 19:31:06 -0500

    Update file_env.rs
    - Added Documentation

--- src/test_tools/file_env.rs
+++ src/test_tools/file_env.rs
@@ -1,9 +1,17 @@
 use std::{fs::{File, self}, io::Write};
 
+/// Struct representing a file environment
 pub struct FileEnv{
+    /// The name of the file
     file_name: String
 }
 impl FileEnv{
+    /// Creates a new FileEnv with the given file name and content
+    ///
+    /// # Arguments
+    ///
+    /// * `file_name` - The name of the file to create
+    /// * `content` - The content to write to the file
     pub fn new(file_name: &str, content: &str) -> FileEnv {
 
         let mut file = File::create(&file_name).unwrap();
@@ -13,6 +21,7 @@ impl FileEnv{
     }
 }
 impl Drop for FileEnv{
+    /// Deletes the file when the FileEnv is dropped
     fn drop(&mut self) {
         println!("Dropping {}", &self.file_name);
         fs::remove_file(&self.file_name).unwrap();
