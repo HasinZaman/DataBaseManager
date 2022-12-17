098695f2478d1d1d85715e919855a73fe5bc1d85
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-12-16 19:31:01 -0500

    Update db_env.rs
    - Added Documentation

--- src/test_tools/db_env.rs
+++ src/test_tools/db_env.rs
@@ -1,9 +1,17 @@
 use crate::backend::{sql::SQL, data_base::DataBase};
 
+/// Struct representing a database environment
 pub struct DbEnv{
+    /// A list of SQL commands to be executed when the environment is reset
     undo: Vec<SQL>
 }
 impl DbEnv{
+    /// Creates a new DbEnv with the given setup and undo commands
+    ///
+    /// # Arguments
+    ///
+    /// * `set_up` - A list of SQL commands to be executed when the environment is set up
+    /// * `undo` - A list of SQL commands to be executed when the environment is dropped from memory
     pub fn new(set_up: Vec<SQL>, undo: Vec<SQL>) -> DbEnv {
         let tmp = DbEnv { undo: undo };
         
@@ -15,6 +23,7 @@ impl DbEnv{
     }
 }
 impl Drop for DbEnv{
+    /// Resets the database environment by executing the undo commands
     fn drop(&mut self) {
         let db = DataBase::from_env().unwrap();
 
