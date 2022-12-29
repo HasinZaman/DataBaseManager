ea5020183a4406a1fd6770c88fc0276409d94abb
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-12-28 22:45:07 -0500

    Update relation_page.rs
    - Extracted match branches into functions
    - View statement line break's at end of line

--- src/ui/pages/schema/relation_page.rs
+++ src/ui/pages/schema/relation_page.rs
@@ -23,82 +23,13 @@ impl Renderable for RelationPage{
 
                 //define relation definition lines
                 match &self.relation {
-                    Relation::Table(table) => {
-                        table.attributes
-                            .iter()
-                            .enumerate()
-                            .map(|(index, attr)| {
-                                Spans::from(vec![ 
-                                    {
-                                        if table.attributes.len() - 1 == index {//last attribute
-                                            Span::from("└")
-                                        }
-                                        else{
-                                            Span::from("├")
-                                        }
-                                    },
-                                    Span::from(" "),
-
-                                    Span::from(attr.name.clone()),
-                                    Span::from(" "),
-
-                                    Span::from(attr.data_type.to_string()),
-                                    Span::from(" "),
-
-                                    {
-                                        if Some(index) == table.primary_key {
-                                            Span::from("Primary Key ")
-                                        }
-                                        else{
-                                            Span::from("")
-                                        }
-                                    },
-                                    Span::from({
-                                        let tmp: Vec<String> = attr.constraint
-                                            .iter()
-                                            .map(|c| format!("{}", c.to_string()))
-                                            .collect();
-
-                                        tmp.join(", ")
-                                    })
-
-                                ])
-                            })
-                            .collect()
-                    },
-                    Relation::View(view) => {
-                        vec![
-                            Spans::from(
-                                {
-                                    let mut words: Vec<Span> = view.query.to_string()
-                                        .split(" ")
-                                        .map(
-                                            |str: &str| {
-                                                Span::from(str.to_string())
-                                            }
-                                        )
-                                        .collect();
-                                    
-                                    {
-                                        let size = words.len()-1;
-                                        (0..size)
-                                        .for_each(
-                                            |i1| {
-                                                words.insert(size - i1, Span::from(" "))
-                                            }
-                                        );
-                                    }
-
-                                    words.insert(0, Span::from("└ "));
-
-                                    words
-                                }
-                            )
-                        ]
-                    }
+                    Relation::Table(table) => table_ui(table),
+                    Relation::View(view) => view_ui(display_area, view)
                 }
             ].concat()
         };
+
+        
 
         frame.render_widget(
             Paragraph::new(lines)
@@ -107,4 +38,80 @@ impl Renderable for RelationPage{
             display_area
         );
     }
+}
+
+fn view_ui(display_area: Rect, view: &crate::backend::relation::view::View) -> Vec<Spans> {
+    let max_width = display_area.width - 2 - 2;
+    let tmp = view.query.to_string()
+        .split(" ")
+        .map(
+            |str: &str| {
+                Span::from(str.to_string())
+            }
+        )
+        .fold(
+            vec![Spans::default()],
+            |lines, word| {
+                let mut lines = lines;
+                let mut last =  lines.pop().unwrap();
+
+                if last.width() + word.width() + 1 < max_width as usize {
+                    last.0.push(Span::from(" "));
+                    last.0.push(word);
+                    lines.push(last);
+                }
+                else {
+                    lines.push(last);
+                    let last = Spans::from(vec![Span::from(" "), word]);
+                    lines.push(last);
+                }
+                
+                lines
+            }
+        );
+    tmp
+}
+
+fn table_ui(table: &crate::backend::relation::table::Table) -> Vec<Spans> {
+    table.attributes
+        .iter()
+        .enumerate()
+        .map(|(index, attr)| {
+            Spans::from(vec![ 
+                {
+                    if table.attributes.len() - 1 == index {//last attribute
+                        Span::from("└")
+                    }
+                    else{
+                        Span::from("├")
+                    }
+                },
+                Span::from(" "),
+
+                Span::from(attr.name.clone()),
+                Span::from(" "),
+
+                Span::from(attr.data_type.to_string()),
+                Span::from(" "),
+
+                {
+                    if Some(index) == table.primary_key {
+                        Span::from("Primary Key ")
+                    }
+                    else{
+                        Span::from("")
+                    }
+                },
+                Span::from({
+                    let tmp: Vec<String> = attr.constraint
+                        .iter()
+                        .map(|c| format!("{}", c.to_string()))
+                        .collect();
+
+                    tmp.join(", ")
+                })
+
+            ])
+        })
+        .collect()
 }
